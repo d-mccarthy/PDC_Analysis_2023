f4fd38d08fde0c2e0db59d2da7869eb4c55fc552
Harry <hlewis@triumf.ca>
2023-11-16 10:06:34 -0800

    New branch for use with data from VERA

--- PDC_DarkAnalysis.C
+++ PDC_DarkAnalysis.C
@@ -25,7 +25,7 @@ using namespace std;
 // Global Variables
 const int fileNumber = 5; // will need to change number of filenames in files array to match (should do as a vector, but root was seg faulting for me)
 double pulseWidth[5] = {83.0,83.0,83.0,83.0,83.0}; //in ADC counts -- 50 ns pulses are 25 ADC
-double binArray[91];
+double binArray[91]; //defines total number of bins
 
 
 //define a class to determine the temperature dependant data (i.e. fit end ranges, VOVs measured)
@@ -46,71 +46,71 @@ public:
 };
 
 // Function to give the afterpulsing distribution with the probability distribution function of the total DNR as input.
-vector<TH1F*> unShadower(vector<TH1F*> inputHist, vector<double> VoV, vector<float> fitStart, vector<float> fitEnd){
+// vector<TH1F*> unShadower(vector<TH1F*> inputHist, vector<double> VoV, vector<float> fitStart, vector<float> fitEnd){
 
-    //define the binning of the histogram. This was done by eye to give reasonable statistics in each bin.
-    for(int j =0; j<91; j++){
+//     //define the binning of the histogram. This was done by eye to give reasonable statistics in each bin.
+//     for(int j =0; j<91; j++){
 
-        binArray[j]= ((pow(2.7162,j*0.25)*1.1)/1E7);    
+//         binArray[j]= ((pow(2.7162,j*0.25)*1.1)/1E7);    
         
-    }
-    // initialize a vector of histograms to give as output
-    std::vector< TH1F* > unShadowed;
-    // initialize variables: following from 
-    double lambda;
-    double beta;
-    double sigPi;
-    double sigLambda;
-    double sigBeta;
-    double Pi;
-    double PiWidth;
-    int numBins;
+//     }
+//     // initialize a vector of histograms to give as output
+//     std::vector< TH1F* > unShadowed;
+//     // initialize variables: following from 
+//     double lambda;
+//     double beta;
+//     double sigPi;
+//     double sigLambda;
+//     double sigBeta;
+//     double Pi;
+//     double PiWidth;
+//     int numBins;
 
-    for(int hInx = 0; hInx<inputHist.size(); hInx++){
-        unShadowed.push_back(new TH1F(Form("unShadowedHist%d",hInx),Form("VoV = %f [V]",VoV[hInx]),90,binArray));
-        beta=0.;
-        sigBeta=0.;
-        numBins = inputHist[hInx]->GetNbinsX();
+//     for(int hInx = 0; hInx<inputHist.size(); hInx++){
+//         unShadowed.push_back(new TH1F(Form("unShadowedHist%d",hInx),Form("VoV = %f [V]",VoV[hInx]),90,binArray));
+//         beta=0.;
+//         sigBeta=0.;
+//         numBins = inputHist[hInx]->GetNbinsX();
 
 
-        for(int ti = 0; ti < numBins-5 ; ti++){
+//         for(int ti = 0; ti < numBins-5 ; ti++){
 
-            Pi = inputHist[hInx]->GetBinContent(ti);
+//             Pi = inputHist[hInx]->GetBinContent(ti);
 
-            sigPi = (inputHist[hInx]->GetBinError(ti));
+//             sigPi = (inputHist[hInx]->GetBinError(ti));
            
-            lambda = -log(1-(Pi/exp(-beta)));
+//             lambda = -log(1-(Pi/exp(-beta)));
 
-            sigLambda = sqrt(sigPi*sigPi+Pi*Pi*sigBeta*sigBeta)/(exp(-beta)-Pi);
-            sigBeta = sqrt(sigPi*sigPi+exp(-2*beta)*sigBeta*sigBeta)/(exp(-beta)-Pi);
+//             sigLambda = sqrt(sigPi*sigPi+Pi*Pi*sigBeta*sigBeta)/(exp(-beta)-Pi);
+//             sigBeta = sqrt(sigPi*sigPi+exp(-2*beta)*sigBeta*sigBeta)/(exp(-beta)-Pi);
             
-            beta += lambda;
+//             beta += lambda;
 
-            if(!isnan(lambda) && sigLambda < lambda){
-                unShadowed[hInx]->SetBinContent(ti,lambda);
-                unShadowed[hInx]->SetBinError(ti,sigLambda);
-            }
+//             if(!isnan(lambda) && sigLambda < lambda){
+//                 unShadowed[hInx]->SetBinContent(ti,lambda);
+//                 unShadowed[hInx]->SetBinError(ti,sigLambda);
+//             }
             
-        }
-        unShadowed[hInx]->Sumw2();
-        unShadowed[hInx]->Scale(1./unShadowed[hInx]->Integral());  
-        unShadowed[hInx]->Scale(1.,"width");
-        unShadowed[hInx]->Fit("pol0","WL","",fitStart[hInx],fitEnd[hInx]);
+//         }
+//         unShadowed[hInx]->Sumw2();
+//         unShadowed[hInx]->Scale(1./unShadowed[hInx]->Integral());  
+//         unShadowed[hInx]->Scale(1.,"width");
+//         unShadowed[hInx]->Fit("pol0","WL","",fitStart[hInx],fitEnd[hInx]);
 
-        unShadowed[hInx]->GetYaxis()->SetTitle("Pulse Rate [Hz/m^{2}]");
-        unShadowed[hInx]->GetXaxis()->SetTitle("Time After Primary Pulse [s]");
-    }
+//         unShadowed[hInx]->GetYaxis()->SetTitle("Pulse Rate [Hz/m^{2}]");
+//         unShadowed[hInx]->GetXaxis()->SetTitle("Time After Primary Pulse [s]");
+//     }
 
-    return unShadowed;
+//     return unShadowed;
         
-}
+// }
 //for use in dealing with back to back pulses
-void filler(int repeats, TH1F* toFill, float width){
-    for(int inx = 0; inx<repeats;inx++){
-        toFill->Fill(width*(2.0/1E9));
-    }
+// void filler(int repeats, TH1F* toFill, float width){
+//     for(int inx = 0; inx<repeats;inx++){
+//         toFill->Fill(width*(2.0/1E9));
+//     }
     
-}
+// }
 
 vector<TH1F*> histCollector(vector<double> VoV, const char *filenames[], int temp, int inSize)
 {
@@ -187,7 +187,9 @@ vector<TH1F*> histCollector(vector<double> VoV, const char *filenames[], int tem
                 
                 //hist[i]->Fill(myTIME[j]);
                 hist[i]->Fill(timeBetween);
-
+                
+                //This is for resolving back-to-back pulses in the case where there are no overlapping pulses (1 SPAD)
+                /*********************
                 if(myWIDTH[j]>(7*pulseWidth[i]+5))
                 {
                     filler(7,hist[i],pulseWidth[i]);
@@ -226,13 +228,14 @@ vector<TH1F*> histCollector(vector<double> VoV, const char *filenames[], int tem
                     timeNow += pulseWidth[i]*(2.0/1E9);
 
                 }
-
+                **********************************************/
                 timeBefore = timeNow;
                 trigLast = myTrigTIME[j];
                 
             }
             
         }   
+
         //get error per each bin and then scale each bin by its width to normalize
         hist[i]->Sumw2();
         //hist[i]->Scale(1.,"width");
@@ -248,13 +251,16 @@ vector<TH1F*> histCollector(vector<double> VoV, const char *filenames[], int tem
 }
 //input should be (csv run numbers, anode)
 void PDC_DarkAnalysis(
-    int inRun1 = 770,
-    int inRun2 = 771,
-    int inRun3 = 772,
-    int inRun4 = 773,
-    int inRun5 = 774,
+    int inRun1 = 2298,
+    int inRun2 = 2299,
+    int inRun3 = 2300,
+    int inRun4 = 2301,
+    int inRun5 = 2302,
     int inSPAD = 7, 
     int inTemp = 293)
+    
+    // 440nm IR-VIS PDE runs
+    //'R02298','R02299','R02300','R02301','R02302'
 {
 
     //store the fits in a vector to retrieve later for error propagation
@@ -265,7 +271,7 @@ void PDC_DarkAnalysis(
     
     //create a look up to find the correct fit range for each temperature
     //fit ranges given in seconds, temperatures in K. Temp, VoV,FitStart,FitEnd
-    tempData.push_back(TemperatureDependentData(293,{1,2,3,4,5},{3E-4,2E-4,1E-4,1E-4,1E-4},{2E-2,1.5E-2,1E-2,9E-3,8E-3}));
+    tempData.push_back(TemperatureDependentData(293,{1,2,3,4,5},{5e-7,5e-7,1E-4,1E-4,1E-4},{2E-2,1.5E-2,1E-2,9E-3,8E-3}));
     tempData.push_back(TemperatureDependentData(273,{1,2,3,4,5},{1E-3,1E-3,9E-4,8E-4,8E-4},{9E-2,7E-2,5E-2,3E-2,2E-2}));
     tempData.push_back(TemperatureDependentData(223,{4,5,6,7,8},{3E-3,4E-3,2E-3,1.3E-3,1E-3},{2E-1,1.8E-1,9E-2,7E-2,4E-2}));
     tempData.push_back(TemperatureDependentData(193,{4,5,6,7,8},{3E-3,2.8E-3,2.4E-3,2.2E-3,2E-3},{1.4E-1,1E-1,9E-2,7E-2,5E-2}));
@@ -324,11 +330,11 @@ void PDC_DarkAnalysis(
     // initialize files
     const char *files[fileNumber] = 
     {
-    Form("root_output_files/output00%d.root",dataNumbers[0]),
-    Form("root_output_files/output00%d.root",dataNumbers[1]),
-    Form("root_output_files/output00%d.root",dataNumbers[2]),
-    Form("root_output_files/output00%d.root",dataNumbers[3]),
-    Form("root_output_files/output00%d.root",dataNumbers[4])
+    Form("root_output_files/output0%d.root",dataNumbers[0]),
+    Form("root_output_files/output0%d.root",dataNumbers[1]),
+    Form("root_output_files/output0%d.root",dataNumbers[2]),
+    Form("root_output_files/output0%d.root",dataNumbers[3]),
+    Form("root_output_files/output0%d.root",dataNumbers[4])
     };
 
     double overVolErrors[fileNumber] = {0.1,0.1,0.1,0.1,0.1};
@@ -336,8 +342,8 @@ void PDC_DarkAnalysis(
     
     
     vector<TH1F*> histograms = histCollector(overV, files, temp, fileNumber);
-    vector<TH1F*> histogramsUnshadowed = unShadower(histograms, overV,fitStartRange,fitEndRange);
-    vector<TH1F*> histogramsUnshadowedSubtracted;
+    //vector<TH1F*> histogramsUnshadowed = unShadower(histograms, overV,fitStartRange,fitEndRange);
+    //vector<TH1F*> histogramsUnshadowedSubtracted;
 
     // output file
 
@@ -347,10 +353,13 @@ void PDC_DarkAnalysis(
 
         //Scale to plot expo fit nicely
         histograms[count]->Scale(1.,"width");
-        histograms[count]->Fit("expo","WL","",fitStartRange[count],fitEndRange[count]*7); // L specifies log likelihood (which deals with the non-gaussian bin statistics in low count bins). We only fit on long time scales to ignore afterpulsing. 
+        //histograms[count]->Fit("expo","WL","",fitStartRange[count],fitEndRange[count]*7); // L specifies log likelihood (which deals with the non-gaussian bin statistics in low count bins). We only fit on long time scales to ignore afterpulsing. 
+        
+        //USING FIXED START END END POINTS FOR FIT
+        histograms[count]->Fit("expo","WL","",5e-6,1e-5); // L specifies log likelihood (which deals with the non-gaussian bin statistics in low count bins). We only fit on long time scales to ignore afterpulsing. 
 
         fit.push_back(histograms[count]->GetFunction("expo")); // save the fit paramters to a vector
-        fitUS.push_back(histogramsUnshadowed[count]->GetFunction("pol0"));
+        //fitUS.push_back(histogramsUnshadowed[count]->GetFunction("pol0"));
 
         //write histo and fit
         histograms[count]->Write(Form("Hist%d_anode%d",count,anode));
@@ -429,38 +438,38 @@ void PDC_DarkAnalysis(
 
 
    //Afterpulsing plot
-    auto c3 = new TCanvas("c3","Unshadowed Distributions",200,10,700,500);
-    c3->SetFillColor(0);
-    c3->SetGridx();
-    c3->SetGridy();
-    c3->GetFrame()->SetFillColor(21);
-    c3->GetFrame()->SetBorderSize(12);
+    // auto c3 = new TCanvas("c3","Unshadowed Distributions",200,10,700,500);
+    // c3->SetFillColor(0);
+    // c3->SetGridx();
+    // c3->SetGridy();
+    // c3->GetFrame()->SetFillColor(21);
+    // c3->GetFrame()->SetBorderSize(12);
 
-    gPad->SetLogy();
-    gPad->SetLogx();  
+    // gPad->SetLogy();
+    // gPad->SetLogx();  
 
 
     float currentVal;
     float currentErr;
 
-    for (int i = 0; i < fileNumber; i++)
-    {
-        histogramsUnshadowedSubtracted.push_back(new TH1F(Form("unShadowedHistSubtracted%d",i),Form("VoV = %f [V]",overV[i]),90,binArray));
-        for (int ti = 0; ti < histogramsUnshadowed[0]->GetNbinsX();ti++)
-        {
-            currentVal = histogramsUnshadowed[i]->GetBinContent(ti);
-            currentErr = histogramsUnshadowed[i]->GetBinError(ti);
-            histogramsUnshadowedSubtracted[i]->SetBinContent(ti, currentVal - fitRateUS[i]);
-            histogramsUnshadowedSubtracted[i]->SetBinError(ti,pow(pow(currentErr,2)+pow(fitErrorUS[i],2),.5));
-        }
-    }
+    // for (int i = 0; i < fileNumber; i++)
+    // {
+    //     histogramsUnshadowedSubtracted.push_back(new TH1F(Form("unShadowedHistSubtracted%d",i),Form("VoV = %f [V]",overV[i]),90,binArray));
+    //     for (int ti = 0; ti < histogramsUnshadowed[0]->GetNbinsX();ti++)
+    //     {
+    //         currentVal = histogramsUnshadowed[i]->GetBinContent(ti);
+    //         currentErr = histogramsUnshadowed[i]->GetBinError(ti);
+    //         histogramsUnshadowedSubtracted[i]->SetBinContent(ti, currentVal - fitRateUS[i]);
+    //         histogramsUnshadowedSubtracted[i]->SetBinError(ti,pow(pow(currentErr,2)+pow(fitErrorUS[i],2),.5));
+    //     }
+    // }
     
 
-    histogramsUnshadowed[0]->Draw("PLC PMC");
-    histogramsUnshadowed[1]->Draw("same PLC PMC");
-    histogramsUnshadowed[2]->Draw("same PLC PMC");
-    histogramsUnshadowed[3]->Draw("same PLC PMC");
-    histogramsUnshadowed[4]->Draw("same PLC PMC");
+    // histogramsUnshadowed[0]->Draw("PLC PMC");
+    // histogramsUnshadowed[1]->Draw("same PLC PMC");
+    // histogramsUnshadowed[2]->Draw("same PLC PMC");
+    // histogramsUnshadowed[3]->Draw("same PLC PMC");
+    // histogramsUnshadowed[4]->Draw("same PLC PMC");
 
     // histogramsUnshadowedSubtracted[0]->Draw("hist PLC PMC");
     // histogramsUnshadowedSubtracted[1]->Draw("same hist PLC PMC");
@@ -479,7 +488,7 @@ void PDC_DarkAnalysis(
 
     for (int i = 0; i < fileNumber; i++)
     {   
-        integrals[i] = histogramsUnshadowedSubtracted[i]->IntegralAndError(0,histogramsUnshadowedSubtracted[i]->FindBin(1E-5),intError[i],"width");
+        //integrals[i] = histogramsUnshadowedSubtracted[i]->IntegralAndError(0,histogramsUnshadowedSubtracted[i]->FindBin(1E-5),intError[i],"width");
         intError[i] = intError[i];
     }
     cout<<"afterpulsing and error at 4 VoV: "<<endl;
